75f6d1387725675b59b0d1388cc14de5354bae87
bishoykamal <bishoykamal95@gmail.com>
2022-09-06 23:53:00 +0200

    delete unused lines

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-# g++ -g -Wall -std=c++11 -o main -static main.cpp -lamqpcpp -lpthread -luv -lev -ldl -pthread
-# ./main
-
-
-
-
 /usr/bin/c++ -o main.o -c main.cpp
 
 /usr/bin/c++ -rdynamic main.o  -o main.exe -lamqpcpp -lev -lpthread -ldl -lssl
